27f796ad15d993b9c656051d19e4003e850fb946
rpsrivastava24337 <rpsrivastava24337@gmail.com>
2023-09-20 02:53:01 +0530

    Add Hashing Password

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //jshint esversion:6
-import 'dotenv/config'
+//import 'dotenv/config'
 import ejs from "ejs";
 import express from "express"; ``
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import encrypt from 'mongoose-encryption';
+import md5 from "md5";
+//import encrypt from 'mongoose-encryption';
 
 const app = express()
 
@@ -21,9 +22,8 @@ const userSchema = new mongoose.Schema({ // set new mongoose schema for encrypti
     password: String
 });
 
-const secret = "abcdefghijklmnopqrstuvwxyz";
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']}) // use encryptedfield to specife which field you like to encrypt
-
+// const secret = "abcdefghijklmnopqrstuvwxyz";
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']}) // use encryptedfield to specife which field you like to encrypt
 const user = new mongoose.model("User", userSchema)
 
 app.get("/", function (req, res) {
@@ -42,7 +42,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
     const newUser = new user({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
     newUser.save()
     res.render("secrets")
@@ -51,7 +51,7 @@ app.post("/register", function (req, res) {
 app.post("/login", async function (req, res) {
 
     const name = req.body.username
-    const pass = req.body.password
+    const pass = md5(req.body.password)
 
     user.findOne({ email: name }).then((founduser) => {
 
