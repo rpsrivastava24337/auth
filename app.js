ebd033ab9ba5188039bd8fc970f587d394b47ca8
rpsrivastava24337 <rpsrivastava24337@gmail.com>
2023-09-21 10:06:49 +0530

    add cookies , session and authentication through the packages that are: passport passport-local passport-local-mongoose express-sessions

--- app.js
+++ app.js
@@ -4,30 +4,39 @@ import ejs from "ejs";
 import express from "express"; ``
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import bcrypt, { hash } from 'bcrypt';
-//import md5 from "md5";
-//import encrypt from 'mongoose-encryption';
+import session from 'express-session';
+import passport from 'passport';
+import passportLocalMongoose from 'passport-local-mongoose';
 
-
-const saltRounds = 10;
 const app = express()
-
 
 app.use(express.static("public"))// The express.static middleware function is used to expose a directory or a file to a particular URL so its contents can be publicly accessed. In this case, the assets directory is exposed to the /assets URL.
 app.set('view engine', 'ejs')//View engines allow us to render web pages using template files. 
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
 
-mongoose.connect("mongodb://127.0.0.1/userDB", { useNewUrlParser: true })
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://127.0.0.1/userDB", { useNewUrlParser: true, useUnifiedTopology: true })
 
 const userSchema = new mongoose.Schema({ // set new mongoose schema for encryption
     email: String,
     password: String
 });
 
-// const secret = "abcdefghijklmnopqrstuvwxyz";
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']}) // use encryptedfield to specife which field you like to encrypt
+userSchema.plugin(passportLocalMongoose)
+
 const user = new mongoose.model("User", userSchema)
+passport.use(user.createStrategy());
+
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
 
 app.get("/", function (req, res) {
     res.render("home")
@@ -42,66 +51,61 @@ app.get("/register", function (req, res) {
     res.render("register")
 })
 
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get('/logout', function (req, res, next) {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
 app.post("/register", function (req, res) {
-    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
 
-        const newUser = new user({
-            email: req.body.username,
-            password: hash
-            // password = md5(req.body.password)
-        })
-        newUser.save()
-        res.render("secrets")
-    })
-
-})
-
-app.post("/login", async function (req, res) {
-
-    const name = req.body.username
-    const pass = req.body.password
-    //const pass = md5(req.body.password)
-
-    user.findOne({ email: name }).then((founduser) => {
-
-        if (founduser) {
-            bcrypt.compare(pass, founduser.password, function (err,result) {
-                if (err){
-                    console.log(err)
-                }
-                if (result !== true) {
-                    res.redirect("/")
-                   
-                }
-                else{
-                    res.render("secrets")
-                }
-
-            })
+    user.register({ username: req.body.username }, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
         }
+    });
 
+});
 
-    })
-})
+///////////////////////////////////////////////////////////////////////////
 
+app.post("/login", function (req, res) {
 
+    const User = new user({
+        username: req.body.username,
+        password: req.body.password
+    });
 
+    req.login(User, function (err) { // res.login from passport .
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
+        }
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 app.listen("3000", function () {
-    console.log("server start")
 
+    console.log("server start")
 
 })
