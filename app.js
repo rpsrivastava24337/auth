1b38fad01744f6929dee25c36c44744e1db1d2df
rpsrivastava24337 <rpsrivastava24337@gmail.com>
2023-09-22 08:24:51 +0530

    Oauth 2.0 google authentication through google

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-//import 'dotenv/config'
+import 'dotenv/config'
 import ejs from "ejs";
 import express from "express"; ``
 import bodyParser from "body-parser";
@@ -7,13 +7,15 @@ import mongoose from "mongoose";
 import session from 'express-session';
 import passport from 'passport';
 import passportLocalMongoose from 'passport-local-mongoose';
+import GoogleStrategy from 'passport-google-oauth20';
+import findOrCreate from 'mongoose-findorcreate';
 
+ 
 const app = express()
 
 app.use(express.static("public"))// The express.static middleware function is used to expose a directory or a file to a particular URL so its contents can be publicly accessed. In this case, the assets directory is exposed to the /assets URL.
-app.set('view engine', 'ejs')//View engines allow us to render web pages using template files. 
-app.use(bodyParser.urlencoded({ extended: true }))
-
+app.set('view engine', 'ejs');//View engines allow us to render web pages using template files. 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(session({
     secret: "Our little secret.",
     resave: false,
@@ -27,21 +29,50 @@ mongoose.connect("mongodb://127.0.0.1/userDB", { useNewUrlParser: true, useUnifi
 
 const userSchema = new mongoose.Schema({ // set new mongoose schema for encryption
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate);
 
 const user = new mongoose.model("User", userSchema)
+
 passport.use(user.createStrategy());
 
-passport.serializeUser(user.serializeUser());
-passport.deserializeUser(user.deserializeUser());
+passport.serializeUser(function(user, done) { done(null, user) });
+passport.deserializeUser(function(user, done) { done(null, user) });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+
+    user.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function (req, res) {
     res.render("home")
 
 })
+
+app.get("/auth/google",
+    passport.authenticate('google', { scope: ["profile"] })
+);
+
+app.get("/auth/google/secrets",
+    passport.authenticate('google', { failureRedirect: "/login" }),
+    function (req, res) {
+        // Successful authentication, redirect to secrets.
+        res.redirect("/secrets");
+    });
 
 app.get("/login", function (req, res) {
     res.render("login")
--- newFile.js
+++
@@ -1,5 +0,0 @@
-import encrypt from 'mongoose-encryption';
-import { userSchema, secret } from "./app";
-
-userSchema.plugin(encrypt, { secret: secret, encryptedField: ['password'] }); // use encryptedfield to specife which field you like to encrypt
-
